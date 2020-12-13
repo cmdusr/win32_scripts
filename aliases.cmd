a8f78066e39073fab0ef0704c727cd31b2f62cad
Robert JK Smith <robertjksmith@live.co.uk>
2020-12-13 12:10:44 +0000

    Reworked dev cmd.

--- aliases.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-doskey vi=nvim $*
-doskey vim=nvim $*
-doskey ls=dir $*
-doskey clear=cls $*
--- setupdevcmd.bat
+++ setupdevcmd.bat
@@ -1,4 +1,7 @@
 @echo off
-call %userprofile%\scripts\aliases.cmd
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-cd %userprofile%\src
+doskey vi=nvim $*
+doskey vim=nvim $*
+doskey ls=dir $*
+doskey clear=cls $*
