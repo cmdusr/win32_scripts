c0c9dd72d1bfcd2f6b48e9ed70231cfd08cc456e
Robert JK Smith <robertjksmith@live.co.uk>
2021-10-03 14:51:00 +0100

    Single daylist

--- daylist.bat
+++ daylist.bat
@@ -1,9 +1,4 @@
 @echo off
 
-set file_path=%userprofile%\dropbox\lists\day
-if not exist %file_path% mkdir %file_path%
-
-for /f %%i in ('getdate.bat') do set today=%%i
-
-rem open in dropbox neovim
-nvim %file_path%/%today%.txt
+set file_path=%userprofile%\dropbox\lists\day.txt
+nvim %file_path%
